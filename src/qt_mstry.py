f73e2664f4d30f24679a433c95df12664ea2997b
Hirsh, Shira <shira.hirsh@intel.com>
2017-08-29 12:20:18 +0300

    indention

--- src/qt_mstry.py
+++ src/qt_mstry.py
@@ -15,26 +15,26 @@ NUM_ITERATIONS = 100
 
 #input_flat - [batch_size, NxN]
 def dqn_ttt(inputs, h1_w_units, h2_w_units)
-    with tf.name_scope('hidden1'):
-      weights = tf.Varible(tf.random_normal([inputs.get_shape().to_list()[-1], h1_w_units]),
-          name='weights')
-      biases = tf.Variable(tf.zeros([h1_w_units]),
-          name='biases')
-      hidden1 = tf.nn.relu(tf.bias_add(tf.matmul(inputs, weights), biases))
+	with tf.name_scope('hidden1'):
+		weights = tf.Varible(tf.random_normal([inputs.get_shape().to_list()[-1], h1_w_units]),
+			name='weights')
+		biases = tf.Variable(tf.zeros([h1_w_units]),
+			name='biases')
+		hidden1 = tf.nn.relu(tf.bias_add(tf.matmul(inputs, weights), biases))
 
     with tf.name_scope('hidden2'):
-      weights = tf.Varible(tf.random_normal([h1_w_units, h2_w_units]),
-                                        name='weights')
-      biases = tf.Variable(tf.zeros([h2_w_units]),
-                      name='biases')
-      hidden2 = tf.nn.relu(tf.bias_add(tf.matmul(hidden1, weights), biases))
+		weights = tf.Varible(tf.random_normal([h1_w_units, h2_w_units]),
+            name='weights')
+		biases = tf.Variable(tf.zeros([h2_w_units]),
+            name='biases')
+		hidden2 = tf.nn.relu(tf.bias_add(tf.matmul(hidden1, weights), biases))
 
     with tf.name_scope('prob_out'):
         weights = tf.Varible(tf.random_normal([h2_w_units, inputs.get_shape().to_list()[-1]),
-        name='weights')
+			name='weights')
         biases = tf.Variable(tf.zeros([inputs.get_shape().to_list()[-1]),
-                                            name='biases')
-      prob_out = tf.softmax(tf.bias_add(tf.matmul(hidden2, weight), biases))
+           name='biases')
+		prob_out = tf.softmax(tf.bias_add(tf.matmul(hidden2, weight), biases))
     return prob_out
 
 def loss(inputs, memory)
@@ -45,34 +45,32 @@ def get_data()
   # Import data   # connect to API 
 
 def main():
-  memory = replay_memory(capcity=REPLAY_MEMORY)
- 
- for i in range(NUM_ITERATIONS)
-    'state', 'action', 'reward', 'next_state', 'done' = board.play()
-# Create the model
+	memory = replay_memory(capcity=REPLAY_MEMORY)
+ 	# Create the model
     x = tf.placeholder(tf.float32, [None, 32])
 
     # Define loss
 
     # Build the graph for the deep net
-  prob_out = dqn_ttt(x)
+	prob_out = dqn_ttt(x)
 
-    with tf.name_scope('loss'):
-        = loss(inputs=prob_out,
-               memory)
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-
-    memory = replay_memory(REPLAY_MEMORY)
-
-    if not done:
+	with tf.Session() as sess:
+			sess.run(tf.global_variables_initializer())
+ 
+	for i in range(NUM_ITERATIONS)
+		'state', 'action', 'reward', 'next_state', 'done' = board.play()
+	
+		with tf.name_scope('loss'):
+			loss = loss(inputs=prob_out,
+				memory)
+		if not done:
             next_state = current_screen - last_screen
         else:
             next_state = None
 	    done = true
 
         # Store the transition in memory
-        memory.push(state, action, next_state, reward)
+        memory.push(state, action, next_state, reward, done)
 
         # Move to the next state
         state = next_state
